852f8e1679f77d7c50d50945a8836b3acae470f3
im_nt <imantahir707@gmail.com>
2025-06-15 01:18:56 +0500

    Add initial database script and AddEmployee class for employee management system

---
+++ Database Script EMS.sql
@@ -0,0 +1,9 @@
+create database employeemanagement;
+use employeemanagement;
+create table login(username varchar(20),password varchar(20));
+select * from login;
+insert into login values ('ImanTahir', 'imantahir157');
+create table employee(name varchar(40), fname varchar(40), dob varchar(40), salary varchar(40),
+address varchar(40),phone varchar(40), email varchar(40), education varchar(40),
+ desination varchar(40),idcard varchar(40),empid varchar(40));
+select * from employee;
